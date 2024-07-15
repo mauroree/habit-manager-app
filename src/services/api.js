215d9d9e1ec37737ae5b1f086b23a450befe62c5
Mauro Ré <82146437+mauroree@users.noreply.github.com>
2024-07-15 20:56:27 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://habit-manager-api-zepu.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
     'auth-token': localStorage.getItem('token')
